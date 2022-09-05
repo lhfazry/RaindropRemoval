3190c564f0892d0c9d92be1f7769668df49a4c75
Fazry <lhuqita.fazry@gmail.com>
2022-09-06 04:20:21 +0700

    add download models

---
+++ download.py
@@ -0,0 +1,13 @@
+#!/opt/conda/bin/python
+from pathlib import Path
+import gdown
+import zipfile
+import os
+
+#Path("logs/LEVIR").mkdir(parents=True, exist_ok=True)
+Path("models").mkdir(exist_ok=True)
+
+os.path.exists()
+
+if not os.path.exists('models/LEVIR_ema_0.9999_650000.pt'):
+    gdown.download(id="1bcnQE5XiU8CMIzzoykQbgW9oFBX3y1xC", output="models/LEVIR_ema_0.9999_650000.pt", quiet=False)
