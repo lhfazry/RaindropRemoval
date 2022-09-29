c7d7e075eda8c6f99f1bd61b47f92697ebfc67c7
Fazry <lhuqita.fazry@gmail.com>
2022-09-29 09:23:22 +0700

    add download.py

--- download.py
+++ download.py
@@ -7,5 +7,5 @@ import os
 #Path("logs/LEVIR").mkdir(parents=True, exist_ok=True)
 Path("models").mkdir(exist_ok=True)
 
-if not os.path.exists('models/LEVIR_ema_0.9999_650000.pt'):
-    gdown.download(id="1bcnQE5XiU8CMIzzoykQbgW9oFBX3y1xC", output="models/LEVIR_ema_0.9999_650000.pt", quiet=False)
+if not os.path.exists('models/RainDrop_ema_0.9999_1000000.pt'):
+    gdown.download(id="1wWMePiseUNCZAD-GuyylwrAmyUtvnPUb", output="models/RainDrop_ema_0.9999_1000000.pt", quiet=False)
