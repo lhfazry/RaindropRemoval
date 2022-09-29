06e4dcf9afa8141d82ee36762f68868e38b82e62
Fazry <lhuqita.fazry@gmail.com>
2022-09-29 16:39:10 +0700

    add conditional sampling

--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -503,7 +503,7 @@ def generate_simplex_noise(
     return noise
 
 def tensors_to_images(tensor):
-    images = ((tensor + 1) * 127.5).clip(0, 255).to(np.uint8)
+    images = ((tensor + 1) * 127.5).clip(0, 255).astype(np.uint8)
     images = images.permute(0, 2, 3, 1)
     images = images.contiguous()
     return images
