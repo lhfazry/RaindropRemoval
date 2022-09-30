888f2c5e806f95dee5e2c577b05fd4b9f89e6e2a
Fazry <lhuqita.fazry@gmail.com>
2022-09-30 14:22:12 +0700

    add conditional sampling

--- clear_gpu.py
+++ clear_gpu.py
@@ -1,10 +1,11 @@
 #!/opt/conda/bin/python
 import torch
 import os
+import gc
 
-torch.cuda.init()
-
-
+gc.collect()
 print(f"Clearing CUDA_ID:{os.environ.get('CUDA_ID')}")
 torch.cuda.empty_cache()
+
+torch.cuda.init()
 print(torch.cuda.memory_summary(int(os.environ.get('CUDA_ID')), True))
