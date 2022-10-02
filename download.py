dfd940e06521a6555968a863b1934b7e28eaa700
Fazry <lhuqita.fazry@gmail.com>
2022-10-02 09:41:29 +0700

    add new model

--- download.py
+++ download.py
@@ -9,3 +9,6 @@ Path("models").mkdir(exist_ok=True)
 
 if not os.path.exists('models/RainDrop_ema_0.9999_1000000.pt'):
     gdown.download(id="1wWMePiseUNCZAD-GuyylwrAmyUtvnPUb", output="models/RainDrop_ema_0.9999_1000000.pt", quiet=False)
+
+if not os.path.exists('models/RainDrop_ema_0.9999_310000.pt'):
+    gdown.download(id="1Rnj2Lia4utHGHYRmI8qO3GNXPV1wjlWh", output="models/RainDrop_ema_0.9999_310000.pt", quiet=False)
